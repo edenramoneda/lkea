f60b10d84d6e6b2007e06370b23797d73acfda7b
Eden Ramoneda <edenlee143@gmail.com>
2020-11-14 00:05:24 +0800

    added map leaflet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,57 @@
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+     <div style="height: 80vh; width: 100%">
+      <div class="info" style="height: 15%">
+        <span>Center: {{ center }}</span>
+        <span>Zoom: {{ zoom }}</span>
+        <span>Bounds: {{ bounds }}</span>
+      </div>
+      <l-map
+        style="height: 80vh; width: 100%"
+        :zoom="zoom"
+        :maxZoom="maxZoom"
+        :center="center"
+        :minZoom="minZoom"
+        :maxBoundsViscosity="maxBoundsViscosity"
+        @update:zoom="zoomUpdated"
+        @update:center="centerUpdated"
+        @update:bounds="boundsUpdated"
+      >
+        <l-tile-layer :url="url" :noWrap="noWrap"></l-tile-layer>
+      </l-map>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    
+    data () {
+      return {
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        zoom: 3,
+        center: [47.413220, -1.219482],
+        bounds: null,
+        maxZoom: 3,
+        minZoom: 2,
+        noWrap: true,
+        maxBoundsViscosity: 1.0
+      };
+    },
+    methods: {
+      doSomethingOnReady() {
+        this.map = this.$refs.myMap.mapObject
+      },
+       zoomUpdated (zoom) {
+      this.zoom = zoom;
+      },
+      centerUpdated (center) {
+        this.center = center;
+      },
+      boundsUpdated (bounds) {
+        this.bounds = bounds;
+      }
     }
-  }
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
